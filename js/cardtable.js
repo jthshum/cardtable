a22925613eaeb1575be1013b71394a005f2e58ab
darrnshn <darren.shen@sydney.edu.au>
2016-03-22 18:27:20 +1100

    Add tapping

--- js/cardtable.js
+++ js/cardtable.js
@@ -14,26 +14,22 @@ function createEmptyStage() {
 function createDragStartHandler(card) {
     return function(event)
     {
-        console.log(event);
-        if (event.data.originalEvent.altKey) {
-            this.tint = 0;
-        } else {
-            // store a reference to the data
-            // the reason for this is because of multitouch
-            // we want to track the movement of this particular touch
-            this.data = event.data;
-            this.alpha = 0.5;
-            this.dragging = true;
+        // store a reference to the data
+        // the reason for this is because of multitouch
+        // we want to track the movement of this particular touch
+        this.data = event.data;
+        this.alpha = 0.5;
+        this.dragging = true;
+        this.dragStartTime = Date.now();
 
-            // Track the mouse position when dragging started
-            this.sx = this.data.getLocalPosition(card).x * card.scale.x;
-            this.sy = this.data.getLocalPosition(card).y * card.scale.y;
+        // Track the mouse position when dragging started
+        this.sx = this.data.getLocalPosition(card).x * card.scale.x;
+        this.sy = this.data.getLocalPosition(card).y * card.scale.y;
 
-            // Bring to front (wtf is life)
-            parent = this.parent;
-            parent.removeChild(this);
-            parent.addChild(this);
-        }
+        // Bring to front (wtf is life)
+        parent = this.parent;
+        parent.removeChild(this);
+        parent.addChild(this);
     }
 }
 
@@ -44,6 +40,11 @@ function onDragEnd() {
 
     // set the interaction data to null
     this.data = null;
+
+    var elapsedTime = Math.abs(Date.now() - this.dragStartTime);
+    if (elapsedTime < 200) { // TODO: check that the mouse hasn't move too far either
+      console.log("tap");
+    }
 }
 
 function onDragMove() {
@@ -68,23 +69,23 @@ function createCard(texture, x, y) {
     var card = new PIXI.Sprite(texture);
 
     card.buttonMode = true; // Show hand cursor on hover
-//    card.interactive = true;
-//
-//    card
-//        // events for drag start
-//        .on('mousedown', createDragStartHandler(card))
-//        .on('touchstart', createDragStartHandler(card))
-//        // events for drag end
-//        .on('mouseup', onDragEnd)
-//        .on('mouseupoutside', onDragEnd)
-//        .on('touchend', onDragEnd)
-//        .on('touchendoutside', onDragEnd)
-//        // events for drag move
-//        .on('mousemove', onDragMove)
-//        .on('touchmove', onDragMove)
-//        // events for mouseover
-//        .on('mouseover', onMouseOver)
-//        .on('mouseout', onMouseOut);
+    card.interactive = true;
+
+    card
+        // events for drag start
+        .on('mousedown', createDragStartHandler(card))
+        .on('touchstart', createDragStartHandler(card))
+        // events for drag end
+        .on('mouseup', onDragEnd)
+        .on('mouseupoutside', onDragEnd)
+        .on('touchend', onDragEnd)
+        .on('touchendoutside', onDragEnd)
+        // events for drag move
+        .on('mousemove', onDragMove)
+        .on('touchmove', onDragMove)
+        // events for mouseover
+        .on('mouseover', onMouseOver)
+        .on('mouseout', onMouseOut);
 
     // Center the sprite's anchor point
     card.anchor.set(0.5);
@@ -104,43 +105,23 @@ function loadCardTable(options) {
 
     document.body.appendChild(renderer.view);
 
-    let scale = scaleToWindow(renderer.view);
-    let t = new Tink(PIXI, renderer.view, scale);
-
-    //TINK Pointer
-    pointer = t.makePointer();
-    pointer.x = pointer.x / scale;
-    pointer.y = pointer.y / scale;
-    //pointer.press = () => console.log("The pointer was pressed");
-    //pointer.release = () => console.log("The pointer was released");
-    //pointer.tap = () => console.log("The pointer was tapped");
-
     stage = createEmptyStage();
 
     // Create the card texture
-
     var texture = PIXI.Texture.fromImage('assets/card.png');
 
     // Keep track of our cards so we can iterate over them later
-    //stage.cards = [];
+    stage.cards = [];
     for (var i = 0; i < 10; i++)
     {
         card = createCard(texture, Math.floor(Math.random() * 800), Math.floor(Math.random() * 600));
-        t.makeDraggable(card);
-        t.makeInteractive(card);
-        card.press = () => console.log("pressed");
-        card.release = () => console.log("released");
-        card.over = () => console.log("over");
-        card.out = () => console.log("out");
-        card.tap = () => console.log("tapped");
-        //stage.cards.push(card);
+        stage.cards.push(card);
         stage.addChild(card);
     }
 
     function animate() {
         requestAnimationFrame(animate);
 
-        t.update();
         // render the container
         renderer.render(stage);
     }
